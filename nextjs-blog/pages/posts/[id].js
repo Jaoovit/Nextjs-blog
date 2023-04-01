9845482bce5c76b07351875c0bd0c315e7e3190b
Jaoovit <jaoovit@gmail.com>
2023-04-01 11:16:09 +0000

    fix an error

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,8 +1,8 @@
-import Date from '../../components/date';
-import Head from 'next/head';
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
-import utilStyles from '../../styles/utils.module.css';
+import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
 
 export default function Post({ postData }) {
   return (
@@ -18,37 +18,19 @@ export default function Post({ postData }) {
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
     </Layout>
-  );
-}
-export default function Post({ postData }) {
-  return (
-    <Layout>
-      {/* Keep the existing code here */}
-
-      {/* Replace {postData.date} with this */}
-      <Date dateString={postData.date} />
-
-      {/* Keep the existing code here */}
-    </Layout>
-  );
+  )
 }
 
-export default function Post({ postData }) {
-  return (
-    <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-    </Layout>
-  );
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
